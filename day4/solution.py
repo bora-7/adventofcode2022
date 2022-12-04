41602d5b978f29a5d83fb3f5385e29de5ad6a3c1
Bora Akyuz <bora.akyuz@warwick.ac.uk>
2022-12-04 05:25:03 +0000

    day4 solution

---
+++ day4/solution.py
@@ -0,0 +1,47 @@
+
+def partOneSolution():
+    count = 0
+    with open('input.txt') as f:
+        for line in f:
+            line = line.rstrip()
+            input = line.split(',')
+            print(input)
+            firstRange = input[0].split('-')
+            print(firstRange)
+            secondRange = input[1].split('-')
+            print(secondRange)
+            
+            if (int(firstRange[0]) <= int(secondRange[0]) and int(firstRange[1]) >= int(secondRange[1])) or (int(secondRange[0]) <= int(firstRange[0]) and int(secondRange[1]) >= int(firstRange[1])):
+                count += 1
+    
+    return count
+
+
+
+def partTwoSolution():
+    count = 0
+    with open('input.txt') as f:
+        for line in f:
+            line = line.rstrip()
+            input = line.split(',')
+            print(input)
+            firstRange = input[0].split('-')
+            print(firstRange)
+            secondRange = input[1].split('-')
+            print(secondRange)
+            
+            if (int(firstRange[0]) <= int(secondRange[0]) and int(firstRange[1]) >= int(secondRange[1])) or (int(secondRange[0]) <= int(firstRange[0]) and int(secondRange[1]) >= int(firstRange[1])):
+                count += 1
+            elif (int(firstRange[0]) >= int(secondRange[0]) and int(firstRange[0]) <= int(secondRange[1])) or (int(secondRange[0]) >= int(firstRange[0]) and int(secondRange[0]) <= int(firstRange[1])):
+                count += 1
+            
+    return count
+
+
+
+def main():
+    #print(partOneSolution())
+    print(partTwoSolution())
+    
+
+main()
